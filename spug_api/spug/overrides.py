6d73f3b6394cd2c94e71270f88abc42748b3602a
zhangchuang <2909290582@qq.com>
2026-02-25 09:55:59 +0800

    启动项目修改配置和修改默认数据库为mysql

---
+++ spug_api/spug/overrides.py
@@ -0,0 +1,17 @@
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spug',             # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
+        'USER': 'spug_user',        # 数据库用户名
+        'PASSWORD': 'spug_passwd',  # 数据库密码
+        'HOST': '127.0.0.1',        # 数据库地址
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'sql_mode': 'STRICT_TRANS_TABLES',
+            #'unix_socket': '/opt/mysql/mysql.sock' # 如果是本机数据库,且不是默认安装的Mysql,需要指定Mysql的socket文件路径
+        }
+    }
+}
